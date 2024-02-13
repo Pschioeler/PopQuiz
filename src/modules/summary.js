cdc6cd81a602e8950c175987458e3f8aa8f2fc94
Nicklas Dauer Pedersen <nick5198@edu.ucl.dk>
2024-02-13 14:37:42 +0100

    added xslt til styling af fil der downloades samt et view der hedder summary.ejs og tilhørende yderligere kode til fil download

--- src/modules/summary.js
+++ src/modules/summary.js
@@ -4,15 +4,54 @@ function displayAnswers(questionCount){
 }
 
 function generateDataForDownload(){
-    //get information about answers and return it for other functions
-    var fileContent = "";
+    //The content in this function must follow xlst file format
+    var fileContent = `
+        <data>
+            <content>Hello, World!</content>
+        </data>
+    `; //this data should arrive from another function as an array
     return fileContent;
 }
 
-function downloadResults(fileName){
-    const fileContent = generateDataForDownload();
+function getXSLTStylesheetToStyleDownloadedFile() {
+    const filePath = "/src/downloadTemplate.xslt"; //modify this path if the xslt file is moved
 
-    const file = new Blob([fileContent], {type: "text/plain"});
+    return fetch(filePath)  // Foretag HTTP-anmodning for at hente XSLT-filen
+        .then(response => response.text())  // Konverter svaret til tekst
+        .catch(error => {
+            console.error('Fejl ved indlæsning af XSLT-filen:', error);
+        });
+}
+
+function transformXMLToXSLFO(xmlData, xsltStylesheet){
+    const xsltProcessor = new XSLTProcessor();
+    const parser = new DOMParser();
+    const xsltDocument = parser.parseFromString(xsltStylesheet, "text/xml");
+    xsltProcessor.importStylesheet(xsltDocument);
+    const xslFO = xsltProcessor.transformToFragment(parser.parseFromString(xmlData, "text/xml"), document);
+
+    return xslFO;
+}
+
+function serializeXSLFOToString(xslFO) {
+    // Konverter XSL-FO til en streng
+    const serializer = new XMLSerializer();
+    const xslFOString = serializer.serializeToString(xslFO);
+    
+    // Returner XSL-FO-strengen
+    return xslFOString;
+}
+
+function downloadResults(fileName){
+    const fileContentAsXML = generateDataForDownload();
+
+    const xsltStyleSheet = getXSLTStylesheetToStyleDownloadedFile();
+
+    const xslFO = transformXMLToXSLFO(fileContentAsXML, xsltStyleSheet);
+
+    const xslFOString = serializeXSLFOToString(xslFO);
+
+    const file = new Blob([xslFOString], {type: "application/pdf"});
     const downloadLink = document.createElement("a");
     const url = URL.createObjectURL(file);
     //Opret et a element og tilknyt fileName og downloadlink til den så brugeren kan aktivere downloaden gennem DOM/Frontend
