2db3c5a1ee0cf8703420e82a3a2f7333042004f5
Nicklas Dauer Pedersen <nick5198@edu.ucl.dk>
2024-02-14 08:43:31 +0100

    Lavede en xslt der formatere indholdet af en txt fil før download. Der mangler stadig konkret svardata som skal anvendes som filindhold

--- src/modules/summary.js
+++ src/modules/summary.js
@@ -2,70 +2,57 @@
 function displayAnswers(questionCount){
     //do some display magic here with each answered question
 }
-
-function generateDataForDownload(){
-    //The content in this function must follow xlst file format
+async function generateDataForDownload() {
+    // The content in this function must follow the XSLT file format
     var fileContent = `
         <data>
             <content>Hello, World!</content>
         </data>
-    `; //this data should arrive from another function as an array
+    `; // This data should arrive from another function as an array
     return fileContent;
 }
+async function getXSLTStylesheetToStyleDownloadedFile() {
+    const filePath = "/Test/downloadTemplate.xslt"; // Just modify this path if the XSLT file is moved
 
-function getXSLTStylesheetToStyleDownloadedFile() {
-    const filePath = "/src/downloadTemplate.xslt"; //modify this path if the xslt file is moved
-
-    return fetch(filePath)  // Foretag HTTP-anmodning for at hente XSLT-filen
-        .then(response => response.text())  // Konverter svaret til tekst
-        .catch(error => {
-            console.error('Fejl ved indlæsning af XSLT-filen:', error);
-        });
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', filePath, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(xhr.responseText);
+                } else {
+                    reject(new Error('Fejl ved indlæsning af XSLT-filen: ' + xhr.statusText));
+                }
+            }
+        };
+        xhr.send();
+    });
 }
-
-function transformXMLToXSLFO(){
-    const xmlData = generateDataForDownload();
-    const xsltStylesheet = getXSLTStylesheetToStyleDownloadedFile();
+async function transformXMLToText(xmlData) {
+    const xsltStylesheet = await getXSLTStylesheetToStyleDownloadedFile();
 
     const xsltProcessor = new XSLTProcessor();
     const parser = new DOMParser();
     const xsltDocument = parser.parseFromString(xsltStylesheet, "text/xml");
     xsltProcessor.importStylesheet(xsltDocument);
-    const xslFO = xsltProcessor.transformToFragment(parser.parseFromString(xmlData, "text/xml"), document);
+    const transformedText = xsltProcessor.transformToFragment(parser.parseFromString(xmlData, "text/xml"), document);
 
-    return xslFO;
+    return transformedText.textContent;
 }
+async function downloadResults() {
+    const fileName = "quiz-results.txt";
+    const xmlData = await generateDataForDownload();
+    const transformedText = await transformXMLToText(xmlData);
 
-function serializeXSLFOToString() {
-    const xslFO = transformXMLToXSLFO();
-    // Konverter XSL-FO til en streng
-    const serializer = new XMLSerializer();
-    const xslFOString = serializer.serializeToString(xslFO);
-    
-    // Returner XSL-FO-strengen
-    return xslFOString;
-}
-
-function downloadResults(fileName){
-    const fileContentAsXML = generateDataForDownload();
-
-    const xsltStyleSheet = getXSLTStylesheetToStyleDownloadedFile();
-
-    const xslFO = transformXMLToXSLFO();
-
-    const xslFOString = serializeXSLFOToString();
-
-    const file = new Blob([xslFOString], {type: "application/pdf"});
+    const file = new Blob([transformedText], { type: "text/plain" });
     const downloadLink = document.createElement("a");
     const url = URL.createObjectURL(file);
-    //Opret et a element og tilknyt fileName og downloadlink til den så brugeren kan aktivere downloaden gennem DOM/Frontend
     downloadLink.href = url;
     downloadLink.download = fileName;
 
     document.body.appendChild(downloadLink);
-    // Simuler klik på downloadlink for at starte download
     downloadLink.click();
-    // Fjern downloadlink fra DOM'en
     document.body.removeChild(downloadLink);
 }
 
